b429c72c3e3c0a6bd8283f20d7e66aba04c82124
Rui Mo <rui.mo@intel.com>
2022-08-23 15:39:26 +0800

    Support selecting subfield from struct (#38)
    Fix bigint range (#40)

--- velox/type/Filter.cpp
+++ velox/type/Filter.cpp
@@ -576,7 +576,6 @@ std::unique_ptr<Filter> createBigintValuesFilter(
       min, max, values, nullAllowed);
 }
 
-
 std::unique_ptr<Filter> createBigintValues(
     const std::vector<int64_t>& values,
     bool nullAllowed) {
