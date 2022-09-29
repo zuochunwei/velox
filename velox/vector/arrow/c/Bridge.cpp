083b502ca44af070d75df39c6bf11e667bdb5ecf
Jin Chengcheng <79615558+jinchengchenghh@users.noreply.github.com>
2022-09-29 10:38:04 +0800

    Support Datetype (#50)
    * support date type
    * support date type serilize

--- velox/vector/arrow/c/Bridge.cpp
+++ velox/vector/arrow/c/Bridge.cpp
@@ -430,6 +430,7 @@ void exportFlat(
     case TypeKind::DOUBLE:
     case TypeKind::SHORT_DECIMAL:
     case TypeKind::LONG_DECIMAL:
+    case TypeKind::DATE:
       exportValues(vec, rows, out, pool, holder);
       break;
     case TypeKind::VARCHAR:
