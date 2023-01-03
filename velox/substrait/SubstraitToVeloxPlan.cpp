a043636292c898e43676c9bc2e3790fc2b3f2d97
zhejiangxiaomai <zhenhui.zhao@intel.com>
2023-01-03 10:03:30 +0800

    rebase 1.3

--- velox/substrait/SubstraitToVeloxPlan.cpp
+++ velox/substrait/SubstraitToVeloxPlan.cpp
@@ -146,8 +146,7 @@ RowTypePtr getJoinOutputType(
   // side.
   bool outputMayIncludeRightColumns =
       !(core::isLeftSemiFilterJoin(joinType) ||
-        core::isLeftSemiProjectJoin(joinType) || core::isAntiJoin(joinType) ||
-        core::isNullAwareAntiJoin(joinType));
+        core::isLeftSemiProjectJoin(joinType) || core::isAntiJoin(joinType));
 
   if (outputMayIncludeLeftColumns && outputMayIncludeRightColumns) {
     return getJoinInputType(leftNode, rightNode);
@@ -335,6 +334,7 @@ core::PlanNodePtr SubstraitVeloxPlanConverter::toVeloxPlan(
     return std::make_shared<core::HashJoinNode>(
         nextPlanNodeId(),
         joinType,
+        joinType == core::JoinType::kNullAwareAnti ? true : false,
         leftKeys,
         rightKeys,
         filter,
