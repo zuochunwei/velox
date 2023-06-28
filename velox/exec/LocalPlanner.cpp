feafeb3d0dbc4eaf558e63d26a8014e8c61414dc
Jin Chengcheng <chengcheng.jin@intel.com>
2023-06-28 11:36:31 +0800

    Remove ValueStream plan node and operator  (#331)

--- velox/exec/LocalPlanner.cpp
+++ velox/exec/LocalPlanner.cpp
@@ -41,7 +41,6 @@
 #include "velox/exec/TopN.h"
 #include "velox/exec/TopNRowNumber.h"
 #include "velox/exec/Unnest.h"
-#include "velox/exec/ValueStream.h"
 #include "velox/exec/Values.h"
 #include "velox/exec/Window.h"
 
@@ -188,9 +187,6 @@ uint32_t maxDrivers(const DriverFactory& driverFactory) {
       if (!values->isParallelizable()) {
         return 1;
       }
-    } else if (std::dynamic_pointer_cast<const core::ValueStreamNode>(node)) {
-      // ValueStream node must run single-threaded.
-      return 1;
     } else if (std::dynamic_pointer_cast<const core::ArrowStreamNode>(node)) {
       // ArrowStream node must run single-threaded.
       return 1;
@@ -407,11 +403,6 @@ std::shared_ptr<Driver> DriverFactory::createDriver(
         auto valuesNode =
             std::dynamic_pointer_cast<const core::ValuesNode>(planNode)) {
       operators.push_back(std::make_unique<Values>(id, ctx.get(), valuesNode));
-    } else if (
-        auto valueStreamNode =
-            std::dynamic_pointer_cast<const core::ValueStreamNode>(planNode)) {
-      operators.push_back(
-          std::make_unique<ValueStream>(id, ctx.get(), valueStreamNode));
     } else if (
         auto arrowStreamNode =
             std::dynamic_pointer_cast<const core::ArrowStreamNode>(planNode)) {
