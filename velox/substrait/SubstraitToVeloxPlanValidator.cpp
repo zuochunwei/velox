386f25efc75ee1f8b159454cec14ce20cfecd6c2
Yang Zhang <34979747+Yohahaha@users.noreply.github.com>
2023-07-05 10:18:47 +0800

    add bin support (#349)

--- velox/substrait/SubstraitToVeloxPlanValidator.cpp
+++ velox/substrait/SubstraitToVeloxPlanValidator.cpp
@@ -1045,6 +1045,7 @@ bool SubstraitToVeloxPlanValidator::validate(
   std::unordered_set<std::string> supportedFuncs = {
       "sum",
       "sum_merge",
+      "bin",
       "collect_set",
       "count",
       "count_merge",
